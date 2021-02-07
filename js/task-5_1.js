15e938094f984c6f327f50deac9538f981df3f95
Oksana Chorna <ksuchorna@gmail.com>
2021-02-07 14:33:10 +0200

    upd

--- js/task-5_1.js
+++ js/task-5_1.js
@@ -8,10 +8,10 @@ Account.prototype.getInfo = function () {
 
 console.log(Account.prototype.getInfo); // function
 
-const mango = new Account("Mangozedog", "mango@dog.woof");
+const mango = new Account('Mangozedog', 'mango@dog.woof');
 
 mango.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
 
-const poly = new Account("Poly", "poly@mail.com");
+const poly = new Account('Poly', 'poly@mail.com');
 
 poly.getInfo(); // Login: Poly, Email: poly@mail.com
